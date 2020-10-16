26912f500782c1075a08d46949e048a75df033d2
Adam Jahr <adamjahr@gmail.com>
2020-10-15 22:07:57 -0400

    added global h1

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <h1>Events For Good</h1>
     <router-view />
   </div>
 </template>
