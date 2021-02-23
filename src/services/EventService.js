3d00807da630d2af0f60c5ab6967e6d127bdf386
bashavmb <basha.vmb@gmail.com>
2021-02-23 03:17:13 -0500

    L6

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'http://my-json-server.typicode.com/bashavmb/vue-realworld-json/',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
