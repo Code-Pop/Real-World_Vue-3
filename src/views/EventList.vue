b584539eaf6494c1fd8f238aa41f84ab86b6bd0c
Kori Riddick <43865262+IvySwing@users.noreply.github.com>
2021-07-04 11:34:08 -0400

    lessons 3 to 6

---
+++ src/views/EventList.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventList',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: null
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
