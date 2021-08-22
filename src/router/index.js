b245ee59abb258821fbed373bc90f2500a134898
Andres Babilonia Torres <eduardobtorrres10@gmail.com>
2021-08-22 16:16:02 -0500

    creando una nueva ruta y de eventlist

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import EventList from '../views/EventList.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/event-list',
+    name: 'Event List',
+    component: EventList
   },
   {
     path: '/about',
