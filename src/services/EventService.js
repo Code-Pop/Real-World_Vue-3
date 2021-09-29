2684bc5de088491632c323ae38be9f7be02ae124
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-09-29 15:33:46 -0300

    Pause

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/IrleySilvestre/repo-vue3',
+  baseURL:
+    'https://my-json-server.typicode.com/IrleySilvestre/Real-World_Vue-3',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
